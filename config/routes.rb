edbb33ed16e3d57fb7170be3a6439532d53e0c7e
EC2 Default User <ec2-user@ip-172-31-46-197.ap-northeast-1.compute.internal>
2022-08-14 13:21:57 +0000

    [update] set root page & pcs routing
    edir routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'pcs/new'
-  get 'pcs/create'
-  get 'pcs/index'
-  get 'pcs/show'
+  root 'pcs#new'
+  resources :pcs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

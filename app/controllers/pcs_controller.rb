123714f349a2133a6029d41573dca1644daad474
EC2 Default User <ec2-user@ip-172-31-46-197.ap-northeast-1.compute.internal>
2022-08-14 14:27:26 +0000

    [add] add pc create feature
    edit pcs_controller.rb

--- app/controllers/pcs_controller.rb
+++ app/controllers/pcs_controller.rb
@@ -4,11 +4,24 @@ class PcsController < ApplicationController
   end
 
   def create
+    @pc = Pc.new(pc_params)
+    @pc.save
+    redirect_to pc_path(@pc.id)
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def pc_params
+    params.require(:pc).permit(:cpu, :cpu_price, :cpu_cooler, :cpu_cooler_price,\
+    :memory, :memory_price, :videocard, :videocard_price, :storage, :storage_price,\
+    :drive, :drive_price, :case, :case_price, :motherboard, :motherboard_price,\
+    :power, :power_price, :system, :system_price, :fan, :fan_price, :other,\
+    :other_price)
   end
 end

16e9bbe8097199775bbbacddef1bf9129339bcb6
EC2 Default User <ec2-user@ip-172-31-46-197.ap-northeast-1.compute.internal>
2022-08-14 15:31:37 +0000

    [add] add pc show page edit show.html & pcs_controller.rb , pc.rb

--- app/controllers/pcs_controller.rb
+++ app/controllers/pcs_controller.rb
@@ -5,6 +5,18 @@ class PcsController < ApplicationController
 
   def create
     @pc = Pc.new(pc_params)
+    @pc.cpu_price = 0 if @pc.cpu_price.nil?
+    @pc.cpu_cooler_price = 0 if @pc.cpu_cooler_price.nil?
+    @pc.memory_price = 0 if @pc.memory_price.nil?
+    @pc.videocard_price = 0 if @pc.videocard_price.nil?
+    @pc.storage_price = 0 if @pc.storage_price.nil?
+    @pc.drive_price = 0 if @pc.drive_price.nil?
+    @pc.case_price = 0 if @pc.case_price.nil?
+    @pc.motherboard_price = 0 if @pc.motherboard_price.nil?
+    @pc.power_price = 0 if @pc.power_price.nil?
+    @pc.system_price = 0 if @pc.system_price.nil?
+    @pc.fan_price = 0 if @pc.fan_price.nil?
+    @pc.other_price = 0 if @pc.other_price.nil?
     @pc.save
     redirect_to pc_path(@pc.id)
   end
@@ -13,6 +25,7 @@ class PcsController < ApplicationController
   end
 
   def show
+    @pc = Pc.find(params[:id])
   end
 
   private
